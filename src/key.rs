74aee4557039ef135d022308c2f1c8f112febe3b
Jayson Lennon <jayson@jaysonlennon.dev>
2026-04-11 21:19:28 -0700

    feat: add <lt> and <gt> special key names for literal angle brackets

--- src/key.rs
+++ src/key.rs
@@ -1,15 +1,15 @@
 // Copyright (C) 2026 Jayson Lennon
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 3 of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, see <https://opensource.org/license/lgpl-3-0>.
 
@@ -81,4 +81,59 @@ pub fn parse_key_sequence<K: Key>(s: &str, leader: &K) -> Vec<K> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crossterm::event::{KeyCode, KeyModifiers};
+
+    use crate::parse_key_sequence;
+
+    fn leader() -> crossterm::event::KeyEvent {
+        crossterm::event::KeyEvent::new(KeyCode::Char('\\'), KeyModifiers::empty())
+    }
+
+    #[test]
+    fn lt_parses_to_literal_less_than() {
+        // Given a key sequence containing <lt>.
+        let keys = parse_key_sequence("<lt>", &leader());
+
+        // Then it produces a single '<' key.
+        assert_eq!(keys.len(), 1);
+        assert_eq!(keys[0].code, KeyCode::Char('<'));
+        assert_eq!(keys[0].modifiers, KeyModifiers::empty());
+    }
+
+    #[test]
+    fn gt_parses_to_literal_greater_than() {
+        // Given a key sequence containing <gt>.
+        let keys = parse_key_sequence("<gt>", &leader());
+
+        // Then it produces a single '>' key.
+        assert_eq!(keys.len(), 1);
+        assert_eq!(keys[0].code, KeyCode::Char('>'));
+        assert_eq!(keys[0].modifiers, KeyModifiers::empty());
+    }
+
+    #[test]
+    fn lt_and_gt_in_mixed_sequence() {
+        // Given a key sequence mixing regular chars with <lt> and <gt>.
+        let keys = parse_key_sequence("a<lt>b<gt>c", &leader());
+
+        // Then it produces the five expected keys.
+        assert_eq!(keys.len(), 5);
+        assert_eq!(keys[0].code, KeyCode::Char('a'));
+        assert_eq!(keys[1].code, KeyCode::Char('<'));
+        assert_eq!(keys[2].code, KeyCode::Char('b'));
+        assert_eq!(keys[3].code, KeyCode::Char('>'));
+        assert_eq!(keys[4].code, KeyCode::Char('c'));
+    }
+
+    #[test]
+    fn consecutive_lt_gt_parses_to_angle_bracket_pair() {
+        // Given a key sequence of <lt><gt>.
+        let keys = parse_key_sequence("<lt><gt>", &leader());
+
+        // Then it produces '<' then '>'.
+        assert_eq!(keys.len(), 2);
+        assert_eq!(keys[0].code, KeyCode::Char('<'));
+        assert_eq!(keys[1].code, KeyCode::Char('>'));
+    }
+}
